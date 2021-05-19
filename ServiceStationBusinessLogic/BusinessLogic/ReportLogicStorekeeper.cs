9640682d90d1891eb3ead45420c3faf1c1ff196a
Nike073i <nike073i@mail.ru>
2021-05-19 18:02:20 +0400

    19.05.2021 18:01 add comments in reportLogicStorekeeper

--- ServiceStationBusinessLogic/BusinessLogic/ReportLogicStorekeeper.cs
+++ ServiceStationBusinessLogic/BusinessLogic/ReportLogicStorekeeper.cs
@@ -20,18 +20,26 @@ namespace ServiceStationBusinessLogic.BusinessLogic
             _technicalMaintenanceStorage = technicalMaintenanceStorage;
             _serviceRecording = serviceRecording;
         }
+
+        /// <summary>
+        /// Метод получения списка машин по выбранным работам
+        /// </summary>
+        /// <param name="selectedWorks"> Список выбранных работы</param>
+        /// <returns>Словарь: key - id машины, value - информация по машине и списку работ</returns>
         private Dictionary<int, ReportCarWorkViewModel> GetCarWork(List<WorkViewModel> selectedWorks)
         {
+            // Получаем список ТО, в которых содержится хотя-бы одна работа из указанных в selectedWorks
             var technicalMaintenances = _technicalMaintenanceStorage.GetFilteredList(new TechnicalMaintenanceBindingModel
             {
-                SelectedWorks = selectedWorks.Select(rec => rec.Id)
-                .ToList()
+                SelectedWorks = selectedWorks.Select(rec => rec.Id).ToList()
             });
             var record = new Dictionary<int, ReportCarWorkViewModel>();
 
             technicalMaintenances.ForEach(rec =>
             {
+                // Берем список машин привязанных к ТО
                 var cars = rec.TechnicalMaintenanceCars.ToList();
+                // Добавляем в словарь машины, если они еще не добавлены
                 cars.Where(car => !record.ContainsKey(car.Key)).ToList()
                 .ForEach(car =>
                 {
@@ -41,9 +49,11 @@ namespace ServiceStationBusinessLogic.BusinessLogic
                         Works = new Dictionary<int, (string, decimal)>()
                     });
                 });
+                // Из привязанных к ТО работ выбираем те, которые указаны в selectedWorks
                 var works = rec.TechnicalMaintenanceWorks
                 .Where(work => selectedWorks.FirstOrDefault(selectedWork => selectedWork.Id == work.Key) != null)
                 .ToList();
+                // К каждой машине из привязанных к ТО добавляем отобранные работы, если они еще не добавлены
                 cars.ForEach(car =>
                 {
                     works.Where(work => !record[car.Key].Works.ContainsKey(work.Key))
@@ -61,13 +71,19 @@ namespace ServiceStationBusinessLogic.BusinessLogic
             return record.OrderBy(rec => rec.Value.CarName).ToDictionary(rec => rec.Key, rec => rec.Value);
         }
 
-        // Получение списка запчастей с указанием работ и машин за определенный период
+        /// <summary>
+        /// Метод получения отчетной информации по движению запчастей за указанный период
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns>Отчет, хранящий информацию по движению запчастей </returns>
         public ReportInfoesStorekeeper GetSparePartWorkCar(ReportStorekeeperBindingModel model)
         {
+            // Получаем список записей сервисов за указанный период
             var serviceRecordings = _serviceRecording.GetFilteredList(new ServiceRecordingBindingModel
             {
                 DateFrom = model.DateFrom,
-                DateTo = model.DateTo
+                DateTo = model.DateTo,
+                ReportStorekeeper = true
             });
 
             var listReportSpareParts = new List<ReportSparePartsViewModel>();
@@ -77,15 +93,17 @@ namespace ServiceStationBusinessLogic.BusinessLogic
                 {
                     Id = serviceRecording.TechnicalMaintenanceId
                 });
+                // Получаем из пройденного ТО только те работы, которые принадлежат текущему пользователю.
                 technicalMaintenances.TechnicalMaintenanceWorks.ToList().Where(rec => model.UserId.HasValue && _workStorage.GetElement(new WorkBindingModel
                 { Id = rec.Key }).UserId == model.UserId.Value)
                 .ToList()
-                .ForEach(technicalMaintenance =>
+                .ForEach(technicalMaintenanceWork =>
                 {
                     var work = _workStorage.GetElement(new WorkBindingModel
                     {
-                        Id = technicalMaintenance.Key
+                        Id = technicalMaintenanceWork.Key
                     });
+                    // Проходимся по запчастям работы и добавляем в словарь
                     work.WorkSpareParts.ToList().ForEach(sparePart =>
                     {
                         listReportSpareParts.Add(new ReportSparePartsViewModel
@@ -100,8 +118,10 @@ namespace ServiceStationBusinessLogic.BusinessLogic
                 });
             });
             var sparePartWorkCar = listReportSpareParts.OrderBy(rec => rec.DatePassed).ThenBy(rec => rec.SparePart).ToList();
+            // Получаем общую информацию по каждой запчасте, группируем по названию запчасти
             var totalCount = listReportSpareParts.GroupBy(sparePart => sparePart.SparePart).Select(rec => new Tuple<string, int>
             (rec.Key, rec.Sum(sparePart => sparePart.Count))).OrderBy(rec => rec.Item1).ToList();
+            // Получаем информацию по помесячному использованию запчастей
             var countByDates = listReportSpareParts.OrderBy(rec => rec.DatePassed).GroupBy(rec => new { rec.DatePassed.Year, rec.DatePassed.Month })
                 .Select(rec => new Tuple<string, int>((string.Format("{0}/{1}", rec.Key.Month, rec.Key.Year)), rec.Sum(sparePart => sparePart.Count))).ToList();
 
